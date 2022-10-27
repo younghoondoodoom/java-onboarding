d681ad2ce9e5ea24021fad803a704f9d38999b56
ChoiYoungHoon <dudgns2865@naver.com>
2022-10-28 02:10:30 +0900

    feat : problem7 사용자와 함께 아는 친구 점수 처리 - 친구 관계 map 생성 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+    public static List<String> solution(String user,
+                                        List<List<String>> friends,
+                                        List<String> visitors) {
+        Map<String, List<String>> relationMap = createRelation(friends);
+        List<String> userRelations = relationMap.get(user);
+
+        return null;
     }
+
+    private static Map<String, List<String>> createRelation(List<List<String>> friends) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (List<String> friend : friends) {
+            addToMap(map, friend.get(0), friend.get(1));
+            addToMap(map, friend.get(1), friend.get(0));
+        }
+        return map;
+    }
+
+    private static void addToMap(Map<String, List<String>> map, String key,
+                                 String value) {
+        if (map.containsKey(key)) {
+            map.get(key).add(value);
+        } else {
+            List<String> list = new ArrayList<>();
+            list.add(value);
+            map.put(key, list);
+        }
+    }
+
 }
