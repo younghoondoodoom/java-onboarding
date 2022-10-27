ff18b762b1409307ff0b2dc46307418d175bc669
ChoiYoungHoon <dudgns2865@naver.com>
2022-10-27 21:58:42 +0900

    feat : 총액 만들기 알고리즘 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,18 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void cntBills(int money, ArrayList<Integer> result) {
+        int[] bills = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        while (money != 0) {
+            for (int i = 0; i < bills.length; i++) {
+                if (money - bills[i] >= 0) {
+                    money -= bills[i];
+                    result.set(i, result.get(i) + 1);
+                    break;
+                }
+            }
+        }
     }
 }
