2c8292b6ef7e9d46bdcd3888ab0aab871a61a9c6
ChoiYoungHoon <dudgns2865@naver.com>
2022-10-27 21:26:08 +0900

    feat : problem2 연속하는 중복 문자를 삭제 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,37 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return removeDup(cryptogram);
+    }
+
+    private static String removeDup(String s) {
+        if (s.isEmpty()) {
+            return s;
+        }
+
+        char prev = 0;
+        boolean flag = false;
+        for (int i = 0; i < s.length(); i++) {
+            int start = i - 1;
+            while (i < s.length() && s.charAt(i) == prev) {
+                i++;
+                flag = true;
+            }
+            if (flag) {
+                if (i >= s.length()) {
+                    s = s.substring(0, start);
+                } else {
+                    s = s.substring(0, start) + s.substring(i);
+                }
+                break;
+            } else {
+                prev = s.charAt(i);
+            }
+        }
+
+        if (flag) {
+            return removeDup(s);
+        }
+        return s;
     }
 }
