1e221aca6bbf314d81ec195f732b87b4feabbcc4
ChoiYoungHoon <dudgns2865@naver.com>
2022-10-27 21:21:30 +0900

    feat : problem1 예외 상황 처리 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,23 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!isValidData(pobi) || !isValidData(crong)) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private static boolean isValidData(List<Integer> list) {
+        if (list.size() != 2) {
+            return false;
+        }
+        if (list.get(0) % 2 != 1 || list.get(1) % 2 != 0) {
+            return false;
+        }
+        if (list.get(0) == 1 || list.get(1) == 400) {
+            return false;
+        }
+        return list.get(0) + 1 == list.get(1);
     }
 }
